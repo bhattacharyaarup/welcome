34d02c9dfae5a558b3ba610dbb3c2f3de82bba39
bhattacharyaarup <arupbhattacharya09@gmail.com>
2022-08-02 17:51:20 +0530

    first commit

---
+++ src/main/java/com/test/welcome/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.test.welcome.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Welcome to spring boot";
+    }
+    
+}
